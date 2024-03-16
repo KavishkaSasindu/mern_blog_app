5ade30cbf199c7a7fc5bb05565fe3f283d999012
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-17 02:52:15 +0530

    updated by  ksdev

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <div>Header Header Header</div>
-    </div>
+    <>
+      <div className="w-[100%] h-[80px] border flex justify-center items-center">
+        <div className="w-[90%] h-full border border-red-950 flex justify-between items-center">
+          <div>Logo</div>
+          <div className="hidden md:flex">
+            <ul className="flex space-x-14">
+              <Link to={"userProfile"}>Profile</Link>
+              <Link to={"author"}>Authors</Link>
+              <Link to={"createPost"}>Create post</Link>
+              <Link to={"logOut"}>Logout</Link>
+            </ul>
+          </div>
+          <div className="md:hidden">
+            <AiOutlineClose />
+            <RxHamburgerMenu />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
