26417c838a054a87d0f2089a39ded1491a5342d4
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-17 21:24:25 +0530

    updated by ksdev

--- frontend/src/components/PostItems.jsx
+++ frontend/src/components/PostItems.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Sunset from "../assets/pexels-brittany-87812.jpg";
+import { Link } from "react-router-dom";
 
 const PostItems = () => {
   return (
     <div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src={Sunset} alt="sunset-mountains" />
+      <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg">
+        <img src={Sunset} alt="sunset-mountains" className="rounded-lg" />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
           <p className="text-gray-700 text-base">
@@ -14,16 +15,27 @@ const PostItems = () => {
             exercitationem praesentium nihil.
           </p>
         </div>
-        <div className="px-6 pt-4 pb-2">
+        <div className="px-6 pt-4 pb-2 flex justify-center items-center space-x-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
+            <div className="flex justify-center items-center space-x-2">
+              <div>
+                <img
+                  src={Sunset}
+                  alt="author"
+                  className="rounded-full h-[30px] w-[30px]"
+                />
+              </div>
+              <div>
+                <h1>Author name</h1>
+              </div>
+            </div>
           </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
-          </span>
+          <Link to={"postDetail/categories/:category"}>
+            {" "}
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {/* {}//add category */} category name
+            </span>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,20 @@ import Sunset from "../assets/pexels-brittany-87812.jpg";
 
 const Home = () => {
   return (
-    <div className="w-[100%] h-screen border mt-5 mb-5 flex justify-center items-center">
-      <div className="w-[90%] h-full border grid  grid-cols-4 ">
-        <PostItems />
-        <PostItems />
-        <PostItems />
-        <PostItems />
-        <PostItems />
+    <div className="w-[100%] h-screen  mt-20  mb-20 flex justify-center items-center scrollbar-hide">
+      <div className="w-[90%] h-screen  flex justify-center items-center">
+        <div className="w-full h-screen overflow-y-scroll scrollbar-hide md:h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9">
+          {/* {getData.posts.map((user)=>{
+      return (postItem eke div eka danna)
+    })} */}
+          <PostItems />
+          <PostItems />
+          <PostItems />
+          <PostItems />
+          <PostItems />
+          <PostItems />
+          <PostItems />
+        </div>
       </div>
     </div>
   );
